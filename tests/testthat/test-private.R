1d656fe1186b3d57d0e672bf401733109e081872
Gowtham Rao <rao@ohdsi.org>
2023-09-11 21:14:51 -0400

    Update test-private.R

--- tests/testthat/test-private.R
+++ tests/testthat/test-private.R
@@ -1,8 +1,6 @@
 testthat::test_that("Execute Cohort Pathways", {
-  testthat::skip_if(condition = skipCdmTests)
-  
   testthat::expect_error(CohortPathways:::createIfNotExist(type = "file"))
   testthat::expect_error(CohortPathways:::createIfNotExist(type = "file", name = NULL))
-  testthat::expect_error(CohortPathways:::createIfNotExist(type = "folder", name = 'xxx'))
-  
+  testthat::expect_no_error(CohortPathways:::createIfNotExist(type = "folder", name = file.path(tempdir(),
+                                                                                                tempfile())))
 })
